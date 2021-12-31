1f4a9dfca7ed7886b53be04b27f479947ebaf636
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-31 16:38:29 -0500

    wraite array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: "input",
+       name: "projectTitle",
+       message: "What is the project title?", 
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: ",
+    },
+    {
+        type: 
+        name: "table of contents",
+        message: "Create a table of contents",
+    },
+    {
+        type: "input"
+        name: "installation",
+        message: "Describe the installation process if any: ",
+    },
+    {
+        type: "input"
+        name: "usage",
+        message: "Provide instructions and examples for use: ",
+    },
+    {
+        type: "input"
+        name: "license",
+        message: "Provide the appropriate license for this project: ",
+    },
+    {
+        type: "input"
+        name: "contributing",
+        message: "Who are the contributors of this project?",
+    },
+    {
+        type: "input"
+        name: "tests",
+        message: "Write tests for your application",
+    },
+    {
+        type: "input"
+        name: "questions",
+        message: "What do I do if I have an issue?",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
