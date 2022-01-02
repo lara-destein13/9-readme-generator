1edb11da3466af283ae9a51a53bc1f345dac3e8b
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-02 14:10:43 -0500

    get questions to appear, prompts user for input, jump to next question

--- Develop/index.js
+++ Develop/index.js
@@ -1,76 +1,74 @@
 // TODO: Include packages needed for this application
-var inquirer = require('inquirer');
-
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questionsArray = [
-    {
-       type: "input",
-       name: "projectTitle",
-       message: "What is the project title?", 
-    },
-    {
+function promptUser(){
+    return inquirer.prompt([
+        {
         type: "input",
-        name: "description",
-        message: "Write a brief description of your project: ",
-    },
-    {
-        type: "input",
-        name: "table of contents",
-        message: "Create a table of contents",
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Describe the installation process if any: ",
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions and examples for use: ",
-    },
-    {
-        type: "input",
-        name: "license",
-        message: "Provide the appropriate license for this project: ",
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Who are the contributors of this project?",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Write tests for your application",
-    },
-    {
-        type: "input",
-        name: "questions",
-        message: "What do I do if I have an issue?",
-    }
-];
+        name: "projectTitle",
+        message: "What is the project title?", 
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: ",
+        },
+        {
+            type: "input",
+            name: "table of contents",
+            message: "Create a table of contents",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for use: ",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Provide the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open" 
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this project?",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Write tests for your application",
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue?",
+        }    
+    ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+        var answers = await promptUser();     
+    }
 
-// Function call to initialize app
+// // Function call to initialize app
 init();
-
-
-
-
-// iterate over the array and print each object using console.dir within a function
-
-
-for (let i=0; i<questionsArray.length; i+=1) {
-    const question = questionsArray[i];
-    console.dir(question);
-    
-    // print out question message and wait for answer. use inquire package
-}
 
 
