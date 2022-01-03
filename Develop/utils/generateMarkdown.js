6c841f19dafc05869d611d6ce4fdc76791d8696b
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-03 15:54:53 -0500

    get prompt working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -16,5 +18,11 @@ function generateMarkdown(data) {
 
 `;
 }
+
+
+var url = `https://img.shields.io/badge/license-apache-brightgreen`;
+var badge = `![badge]${(url)}`;
+console.log(badge);
+
 
 module.exports = generateMarkdown;
