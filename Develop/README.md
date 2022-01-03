488ebca30c538fd5b0e0df88aa1b67a0dcf0a200
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-03 18:46:44 -0500

    work with string interpelation

---
+++ Develop/README.md
@@ -0,0 +1,20 @@
+<h1 align="center">sf 👋</h1>
+
+ ## Description
+ 🔍 sdf
+
+ ## Table of Contents
+ - [Description](#description)
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [License](#license)
+ - [Contribution](#contribution)
+ - [Tests](#tests)
+ - [Questions](#questions)
+
+ sdf
+ sdf
+ Apache
+ sf
+ sdf
+ 
--- Develop/index.js
+++ Develop/index.js
@@ -69,8 +69,58 @@ const questions = [
 // for (var i=0; i< questions.length; i++);
 // const question = questions[i]
 
+function writeTitle(title) {
+    var output = `<h1 align="center">${title} 👋</h1>\n\n `;
+    fs.appendFileSync("README.md", output);
+}
+
+
+function writeDescription(description) {
+    var output = `## Description\n 🔍 ${description}\n\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeTableOfContents(tableOfContents) {
+    var output = `## Table of Contents\n - [Description](#description)\n - [Installation](#installation)\n - [Usage](#usage)\n - [License](#license)\n - [Contribution](#contribution)\n - [Tests](#tests)\n - [Questions](#questions)\n\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeInstallation(installation) {
+    var output = `${installation}\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeUsage(usage) {
+    var output = `${usage}\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeLicense(license) {
+    var output = `${license}\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeContributing(contributing) {
+    var output = `${contributing}\n `
+    fs.appendFileSync("README.md", output);
+}
+
+function writeTests(tests) {
+    var output = `${tests}\n `
+    fs.appendFileSync("README.md", output);
+}
+
 function genMarkdown(answers) {
     console.dir(answers);
+    fs.writeFileSync('README.md', '');
+    writeTitle(answers.projectTitle);
+    writeDescription(answers.description);
+    writeTableOfContents(answers['table of contents']);
+    writeInstallation(answers.installation);
+    writeUsage(answers.usage);
+    writeLicense(answers.license);
+    writeContributing(answers.contributing);
+    writeTests(answers.tests);
 }
 
 inquirer
